b7133b4cb5c0e0fc08757281b5aced388349e30e
John Aisthorpe <john.aisthorpe@gmail.com>
2023-03-13 19:04:33 +0000

    dark mode added to Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,31 +1,94 @@
-import {Box, Flex, HStack, Link, IconButton, Icon, Text, useDisclosure, Button, Stack, useColorModeValue, useColorMode } from '@chakra-ui/react';
-import { Link as ReactLink } from 'react-router-dom';
-import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
-import { GiBookCover } from 'react-icons/gi';
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Icon,
+  Text,
+  useDisclosure,
+  Button,
+  Stack,
+  useColorModeValue,
+  useColorMode,
+  Center,
+} from "@chakra-ui/react";
+import { Link as ReactLink } from "react-router-dom";
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { GiBookCover } from "react-icons/gi";
+import { ReactNode } from "react";
 
+type Link = {
+  linkName: string;
+  path: string;
+};
+
+const links: Link[] = [
+  { linkName: "Books", path: "/books" },
+  { linkName: "shoppingCart", path: "/cart" },
+];
 
 const Navbar: React.FC = () => {
-    const {isOpen, onClose, onOpen} = useDisclosure();
-    const {colorMode, toggleColorMode} = useColorMode();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const NavLink: React.FC<{
+    path: string;
+    children?: React.ReactNode;
+  }> = ({ path, children }) => (
+    <Link
+      as={ReactLink}
+      to={path}
+      px={2}
+      py={2}
+      rounded="md"
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.900"),
+      }}
+    >
+      {children}
+    </Link>
+  );
+
   return (
-    <Box bg= {useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems='center' justifyContent='space-between'>
-            <IconButton aria-label='menuOpen-Close' size='md' icon={isOpen ? <CloseIcon/> : <HamburgerIcon/>} display={{md:'none'}} onClick={isOpen? onClose:onOpen} />
-            <HStack>
-                <Link as={ReactLink} to='/'>
-                    <Flex alignItems='center'>
-                        <Icon as={GiBookCover} h={6} w={6} color="blue.500"/>
-                        <Text fontWeight='semibold'>Capstone Books</Text>
+    <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      <Flex h={16} alignItems="center" justifyContent="space-between">
+        <IconButton
+          aria-label="menuOpen-Close"
+          size="md"
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          display={{ md: "none" }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack>
+          <Link as={ReactLink} to="/">
+            <Flex alignItems="center">
+              <Icon as={GiBookCover} h={6} w={6} color="blue.500" />
+              <Text fontWeight="semibold">Capstone Books</Text>
+            </Flex>
+          </Link>
 
-
-                    </Flex>
-                </Link>
-            </HStack>
+          <HStack>
+            {links.map((link) => (
+              <NavLink key={link.linkName} path={link.path}>
+                {link.linkName}
+              </NavLink>
+            ))}
+          </HStack>
+        </HStack>
+        <Flex alignItems="center">
+          <Icon
+            cursor="pointer"
+            mr="3"
+            as={colorMode === "light" ? MoonIcon : SunIcon}
+            alignSelf="center"
+            onClick={() => toggleColorMode()}
+          />
         </Flex>
-
-
+      </Flex>
     </Box>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
