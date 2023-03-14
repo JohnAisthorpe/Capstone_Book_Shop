2ca2522ed63ca6d01f420d9d6c786fd4e3da0a8f
Marcin <marcinegzmont@gmail.com>
2023-03-14 15:19:36 +0000

    Started on the BookCard

--- client/src/components/BookCard.tsx
+++ client/src/components/BookCard.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import {
+  Flex,
+  Circle,
+  Box,
+  Image,
+  Badge,
+  useColorModeValue,
+  Icon,
+  Button,
+  Tooltip,
+  Stack,
+  Link,
+  HStack,
+  Text,
+} from "@chakra-ui/react";
 
-function BookCard() {
-  return <div>BookCard</div>;
+import { Link as ReactLink } from "react-router-dom";
+import { useState } from "react";
+
+interface Book {
+  _id: number;
+  title: string;
+  author: string;
+  description: string;
+  image: string;
+  category: string;
+  price: number;
+  stock: number;
 }
+interface BookCardProps {
+  book: Book;
+}
+
+const BookCard: React.FC<BookCardProps> = ({ book }) => {
+  return (
+    <Stack
+      p="2"
+      spacing="3px"
+      bg={useColorModeValue("white", "gray.800")}
+      minW="240px"
+      h="450px"
+      rounded="lg"
+      position="relative"
+    >
+      <Image src={book.image} alt={book.title} />
+      <Box></Box>
+    </Stack>
+  );
+};
 
 export default BookCard;
--- client/src/screens/BooksScreen.tsx
+++ client/src/screens/BooksScreen.tsx
@@ -1,19 +1,20 @@
-import { Center, Wrap, WrapItem, Image } from "@chakra-ui/react";
+import { Center, Wrap, WrapItem } from "@chakra-ui/react";
 
 import { books } from "../books";
+import BookCard from "../components/BookCard";
 
-function BooksScreen() {
+const BooksScreen = () => {
   return (
     <Wrap spacing="30px" justify="center" minHeight="100vh">
       {books.map((book) => (
         <WrapItem key={book._id}>
           <Center w="250px" h="550">
-            {book.title}
+            <BookCard book={book} />
           </Center>
         </WrapItem>
       ))}
     </Wrap>
   );
-}
+};
 
 export default BooksScreen;
