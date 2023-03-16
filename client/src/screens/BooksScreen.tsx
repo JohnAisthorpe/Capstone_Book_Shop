0d22e90e154fee9c31e7bfc70e49d2a87bb2627c
Marcin <marcinegzmont@gmail.com>
2023-03-16 14:58:34 +0000

    redux setup

--- client/src/screens/BooksScreen.tsx
+++ client/src/screens/BooksScreen.tsx
@@ -1,5 +1,15 @@
-import { Center, Wrap, WrapItem } from "@chakra-ui/react";
-import { useEffect } from "react";
+import {
+  Center,
+  Wrap,
+  WrapItem,
+  Stack,
+  Spinner,
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+} from "@chakra-ui/react";
+import { ReactNode, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import BookCard from "../components/BookCard";
 import { getBooks } from "../redux/actions/bookActions";
@@ -9,7 +19,6 @@ const BooksScreen = () => {
   const dispatch = useDispatch();
 
   const bookList: BooksState = useSelector(booksSelector);
-
   const { loading, error, books } = bookList;
 
   useEffect(() => {
@@ -18,13 +27,32 @@ const BooksScreen = () => {
 
   return (
     <Wrap spacing="30px" justify="center" minHeight="100vh">
-      {books.map((book) => (
-        <WrapItem key={book._id}>
-          <Center w="250px" h="550">
-            <BookCard book={book} />
-          </Center>
-        </WrapItem>
-      ))}
+      {loading ? (
+        <Stack direction="row" spacing={4}>
+          <Spinner
+            mt={20}
+            thickness="10px"
+            speed="0.65"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        </Stack>
+      ) : error ? (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle>Oops!</AlertTitle>
+          <AlertDescription>{error.toString()}</AlertDescription>
+        </Alert>
+      ) : (
+        books.map((book) => (
+          <WrapItem key={book._id}>
+            <Center w="250px" h="550">
+              <BookCard book={book} />
+            </Center>
+          </WrapItem>
+        ))
+      )}
     </Wrap>
   );
 };
