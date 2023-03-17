df3e160080616d85e9b1abd172e2006ed88fab96
Marcin <marcinegzmont@gmail.com>
2023-03-17 09:38:52 +0000

    added basket route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import BooksScreen from "./screens/BooksScreen";
+import BasketScreen from "./screens/BasketScreen";
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const App: React.FC = () => {
         <main>
           <Routes>
             <Route path="/books" element={<BooksScreen />} />
+            <Route path="/basket" element={<BasketScreen />} />
           </Routes>
         </main>
       </Router>
