b1a1082868f04b19dec169bc6fe536f296e9c3b8
Marcin <marcinegzmont@gmail.com>
2023-03-18 18:26:03 +0000

    User model created

---
+++ server/models/User.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userScheme = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+userScheme.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userScheme.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userScheme);
+export default User;
