41178cd5a0bb697fc6a5030368b9d92e1b8a9892
Marcin <marcinegzmont@gmail.com>
2023-03-19 22:12:10 +0000

    Login screen part 8 finished

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -13,9 +13,22 @@ import {
   useColorMode,
   Center,
   useToast,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
 } from "@chakra-ui/react";
 import { Link as ReactLink } from "react-router-dom";
-import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import {
+  HamburgerIcon,
+  CloseIcon,
+  MoonIcon,
+  SunIcon,
+  ChevronDownIcon,
+} from "@chakra-ui/icons";
+import { CgProfile } from "react-icons/cg";
+import { MdLocalShipping, MdLogout } from "react-icons/md";
 import { GiBookCover } from "react-icons/gi";
 import { ReactNode } from "react";
 import { userSelector } from "../redux/slices/user";
@@ -97,7 +110,28 @@ const Navbar: React.FC = () => {
             onClick={() => toggleColorMode()}
           />
           {userInfo ? (
-            <p>logged in </p>
+            <>
+              <Menu>
+                <MenuButton px="4" py="2" transition="all 0.3s" as={Button}>
+                  {userInfo.name} <ChevronDownIcon />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem as={ReactLink} to="/profile">
+                    <CgProfile />
+                    <Text ml="2">Profile</Text>
+                  </MenuItem>
+                  <MenuItem as={ReactLink} to="/your-orders">
+                    <MdLocalShipping />
+                    <Text ml="2">Your orders</Text>
+                  </MenuItem>
+                  <MenuDivider />
+                  <MenuItem onClick={logoutHandler}>
+                    <MdLogout />
+                    <Text ml="2">Logout</Text>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </>
           ) : (
             <>
               <Button
