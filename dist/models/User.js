41d4ec34effc066f8dd5857857266e374ff570f5
Marcin <marcinegzmont@gmail.com>
2023-03-19 08:20:03 +0000

    register route tested

---
+++ dist/models/User.js
@@ -0,0 +1,42 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
+const userScheme = new mongoose_1.default.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+}, { timestamps: true });
+userScheme.methods.matchPasswords = function (enteredPassword) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield bcryptjs_1.default.compare(enteredPassword, this.password);
+    });
+};
+const User = mongoose_1.default.model("User", userScheme);
+exports.default = User;
---
+++ dist/routes/userRoutes.js
@@ -0,0 +1,74 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const User_1 = __importDefault(require("../models/User"));
+const express_async_handler_1 = __importDefault(require("express-async-handler"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const userRoutes = express_1.default.Router();
+//TODO: redefine expiresIn
+const genToken = (id) => {
+    // we are using the ! operator to tell TypeScript that we are sure that the TOKEN_SECRET environment variable exists and is not undefined.
+    return jsonwebtoken_1.default.sign({ id }, process.env.TOKEN_SECRET, { expiresIn: "60d" });
+};
+const loginUser = (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password } = req.body;
+    const user = yield User_1.default.findOne({ email });
+    if (user && (yield user.matchPasswords(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: genToken(user._id),
+        });
+    }
+    else {
+        //not authenticated
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
+}));
+const registerUser = (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name, email, password } = req.body;
+    const userExist = yield User_1.default.findOne({ email });
+    if (userExist) {
+        res.status(400);
+        throw new Error("We already have an account with that email address.");
+    }
+    const user = yield User_1.default.create({
+        name,
+        email,
+        password,
+    });
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: genToken(user._id),
+        });
+    }
+    else {
+        //bad request
+        res.json(400);
+        throw new Error("Invalid user data.");
+    }
+}));
+userRoutes.route("/login").post(loginUser);
+userRoutes.route("/register").post(registerUser);
+exports.default = userRoutes;
